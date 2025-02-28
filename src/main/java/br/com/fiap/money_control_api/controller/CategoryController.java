e07d7382d4ec8e75dd91a7d6dbf8f293fd8ff3d7
laviniapark <laviniasoo03@gmail.com>
2025-02-28 08:48:03 -0300

    Codigo antes da alteração (armazenamento em lista)

--- src/main/java/br/com/fiap/money_control_api/controller/CategoryController.java
+++ src/main/java/br/com/fiap/money_control_api/controller/CategoryController.java
@@ -1,7 +1,11 @@
 package br.com.fiap.money_control_api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.money_control_api.model.Category;
@@ -9,9 +13,15 @@ import br.com.fiap.money_control_api.model.Category;
 @RestController
 public class CategoryController {
 
-    @RequestMapping(path = "/category", produces = "application/json", method = {RequestMethod.GET})
-	public Category index(){
+	//private List<Category> repository = new ArrayList<>();
+
+	@GetMapping("/categories")
+	public Category index() {
 		return new Category(1L, "Educação", "book");
 	}
-    
+
+	@PostMapping("/categories")
+	public void create(@RequestBody Category category){
+		System.out.println("Cadastrando categoria " + category.getName());
+	}
 }
