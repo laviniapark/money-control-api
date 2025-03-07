fcb488e058b1b713e5dedbf06c430039e0548ad0
laviniapark <laviniasoo03@gmail.com>
2025-03-07 09:30:35 -0300

    AULA 07 03 2025

--- src/main/java/br/com/fiap/money_control_api/controller/CategoryController.java
+++ src/main/java/br/com/fiap/money_control_api/controller/CategoryController.java
@@ -3,24 +3,32 @@ package br.com.fiap.money_control_api.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.fiap.money_control_api.model.Category;
 
 @RestController
+@RequestMapping("/categories") // faz com que todo o controller aponte para o path categories
 public class CategoryController {
+
+	private final Logger log = LoggerFactory.getLogger(getClass());
 
 	private List<Category> repository = new ArrayList<>();
 
-	@GetMapping("/categories") // o padrao 200 - OK será utilizado no GET
+	@GetMapping
 	public List<Category> index() {
 		return repository;
 	}
@@ -33,26 +41,47 @@ public class CategoryController {
 	// 	return category;
 	// }
 
-	@PostMapping("/categories")
+	@PostMapping
 	public ResponseEntity<Category> create(@RequestBody Category category){
-		System.out.println("Cadastrando categoria " + category.getName());
+		log.info("Cadastrando categoria " + category.getName());
 		repository.add(category);
 		return ResponseEntity.status(201).body(category);
 	}
 
-	@GetMapping("/categories/{id}") // o nome do path e da variavel devem ser o mesmo
-	public ResponseEntity<Category> get(@PathVariable Long id){ // precisamos receber o valor atraves do parametro
-		System.out.println("buscando categoria " + id);
-		// programacao funcional do Java para buscar categoria
-		var category = repository.stream()
-			.filter(c -> c.getId().equals(id)) // o filtro foi colocado para nos trazer apenas a info desejada (a regra ficará entre parenteses)
-			.findFirst();
-		
-		//verifica se existe algo 
-		if(category.isEmpty()){
-			return ResponseEntity.notFound().build();
-		}
+	@GetMapping("{id}") // o nome do path e da variavel devem ser o mesmo
+	public Category get(@PathVariable Long id){ // precisamos receber o valor atraves do parametro
+		log.info("buscando categoria " + id);
 
-			return ResponseEntity.ok().body(category.get()); // jamais chame o get sem garantir que o valor esteja presente
+		return getCategory(id);
+	}
+
+	@DeleteMapping("{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void destroy(@PathVariable Long id){
+		log.info("Apagando categoria " + id);
+
+		repository.remove(getCategory(id));
+	}
+
+	@PutMapping("{id}")
+	public Category update(@PathVariable Long id, @RequestBody Category category){ //esse metodo ira receber o id e a categoria com dados atualizados que estarão no body
+		log.info("Atualizando categoria " + id + " para " + category); //sysouts utilizados para saber se o metodo está sendo inicializado corretamente
+
+		//tirar a categoria antiga e colocar a nova
+		repository.remove(getCategory(id)); //agora que temos o metodo getCategory, por ele não ser opcional não é mais necessario o metogo get()
+		category.setId(id);
+		repository.add(category);
+
+		return category; // nao é mais necessario o retorno do 200 pois não há mais a opção de tambem mostrar o retorno 404
+	}
+
+	private Category getCategory(Long id) {
+		return repository.stream()
+			.filter(c -> c.getId().equals(id))
+			.findFirst()
+			.orElseThrow(
+				() -> new ResponseStatusException(HttpStatus.NOT_FOUND)
+			);
+			//exception - forma do metodo avisar que houve um erro
 	}
 }
--- src/main/java/br/com/fiap/money_control_api/model/Category.java
+++ src/main/java/br/com/fiap/money_control_api/model/Category.java
@@ -6,21 +6,33 @@ public class Category {
     private Long id;
     private String name;
     private String icon;
-    
+
     public Category(Long id, String name, String icon) {
-        this.id = Math.abs(new Random().nextLong()); //gerador de numero sendo utilizado temporariamente enquanto nao implementamos o banco de dados
+        this.id = Math.abs(new Random().nextLong()); // gerador de numero sendo utilizado temporariamente enquanto nao
+                                                     // implementamos o banco de dados
         this.name = name;
         this.icon = icon;
     }
+
     public Long getId() {
         return id;
     }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
+
     public String getIcon() {
         return icon;
     }
 
-    
+    @Override
+    public String toString() {
+        return "Category [id=" + id + ", name=" + name + ", icon=" + icon + "]";
+    }
+
 }
